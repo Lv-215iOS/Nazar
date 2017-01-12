07b9954fc726318ad1dfdfca2473bf8c9484233d
Nazar <nazar@Nazars-iMac.local>
2017-01-12 14:26:10 -0800

    Tests

--- MyCalc/MyCalcTests/MyCalcTests.swift
+++ MyCalc/MyCalcTests/MyCalcTests.swift
@@ -59,7 +59,79 @@ class MyCalcTests: XCTestCase {
         XCTAssert(result == 1)
         
     }
+    func testSin3pidiv2() {
+        brain?.digit(value: 4.71238898)
+        brain?.unary(operation: .Sin)
+        var result: Double? = nil
+        brain?.result = {(value, error) in
+            result = value
+        }
+        brain?.utility(operation: .Equal)
+        XCTAssert(result == -1)
+        
+    }
+    
+    func testCospi() {
+        brain?.digit(value: 3.14159265)
+        brain?.unary(operation: .Cos)
+        var result: Double? = nil
+        brain?.result = {(value, error) in
+            result = value
+        }
+        brain?.utility(operation: .Equal)
+        XCTAssert(result == -1)
+        
+    }
+    
+    func testSinpidiv2() {
+        brain?.digit(value: 1.57079632)
+        brain?.unary(operation: .Sin)
+        var result: Double? = nil
+        brain?.result = {(value, error) in
+            result = value
+        }
+        brain?.utility(operation: .Equal)
+        XCTAssert(result == 1)
+        
+    }
+    
+    func testCospidiv2() {
+        brain?.digit(value: 1.57079632679)
+        brain?.unary(operation: .Cos)
+        var result: Double? = nil
+        brain?.result = {(value, error) in
+            result = value
+        }
+        brain?.utility(operation: .Equal)
+        XCTAssert(result == 0)
+        
+    }
 
+    
+    func testSinpidiv6() {
+        brain?.digit(value: 0.523598775598298873)
+        brain?.unary(operation: .Sin)
+        var result: Double? = nil
+        brain?.result = {(value, error) in
+            result = value
+        }
+        brain?.utility(operation: .Equal)
+        XCTAssert(result == 0.5)
+        
+    }
+    
+    func testCospidiv3() {
+        brain?.digit(value: 1.04719755119)
+        brain?.unary(operation: .Cos)
+        var result: Double? = nil
+        brain?.result = {(value, error) in
+            result = value
+        }
+        brain?.utility(operation: .Equal)
+        XCTAssert(result == 0.5)
+        
+    }
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
